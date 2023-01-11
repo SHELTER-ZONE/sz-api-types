59b5ffca2998fec8e313b8401e88a17e4f9dd194
Proladon <Proladon@gmail.com>
2023-01-11 12:10:50 +0800

    [fix] APISZUserProfileCreate required

--- SZUserProfile/index.ts
+++ SZUserProfile/index.ts
@@ -13,12 +13,12 @@ export interface SZUserProfile {
 export interface APISZUserProfileCreate {
   name: string
   userId: string
-  title: string[] | null
-  createdAt: number
-  updatedAt: number
-  rep: number
-  org: any | null
-  role: 'user' | 'admin'
+  createdAt?: number
+  updatedAt?: number
+  title?: string[] | null
+  rep?: number
+  org?: any | null
+  role?: 'user' | 'admin'
 }
 
 export interface APISZUserProfileUpdate {
