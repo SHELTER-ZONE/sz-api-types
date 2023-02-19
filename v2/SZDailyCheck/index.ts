bfdfa96b7932fc97cca95c0b0c932658798d9bbe
Proladon <Proladon@gmail.com>
2023-02-19 18:15:06 +0800

    [v2] update - SZDailyCheck

--- v2/SZDailyCheck/index.ts
+++ v2/SZDailyCheck/index.ts
@@ -4,20 +4,20 @@ export type SZDailyCheckRecord = {
   lastRecord: Date
 }
 export type APISZDailyCheckRecordGet = {
-  userId: string
-}
-export type APISZDailyCheckRecordFind = {
-  userId: string
-}
-export type APISZDailyCheckRecordCreate = {
-  userId: string
   start: number
   limit: number
   sort: 'asc' | 'desc'
   from: string | Date
   end: string | Date
 }
+export type APISZDailyCheckRecordFind = {
+  userId: string
+}
+export type APISZDailyCheckRecordCreate = {
+  userId: string
+}
 export type APISZDailyCheckRecordUpdate = {
+  userId: string
   lastRecord: string | Date
 }
 export type APISZDailyCheckRecordDelete = {
